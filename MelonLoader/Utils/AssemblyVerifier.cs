6c2c3e73a434a8485ffc60080f84e442a798b6fb
nullptr <78525180+unreliablecode@users.noreply.github.com>
2026-06-02 02:36:41 +0700

    Fix formatting in AssemblyVerifier.cs

--- MelonLoader/Utils/AssemblyVerifier.cs
+++ MelonLoader/Utils/AssemblyVerifier.cs
@@ -31,7 +31,8 @@ namespace MelonLoader.Utils
             '(',
             ')',
             '?',
-            '{n            '}',
+            '{', 
+            '}',  
             '!'
         };
 
